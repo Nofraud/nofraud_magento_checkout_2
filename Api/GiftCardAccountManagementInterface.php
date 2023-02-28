5d820b02f457d57a3d00520744f916eac538c90d
sagar <sagar@nexusbox.dev>
2023-02-28 11:10:28 +0000

    worked on the removed gift card api

--- Api/GiftCardAccountManagementInterface.php
+++ Api/GiftCardAccountManagementInterface.php
@@ -6,10 +6,19 @@ interface GiftCardAccountManagementInterface
 {
     /**
      * Add gift card to the cart.
-     *
+     * @param boolean $is_loggedin
      * @param string|int $cartId
      * @param string $giftCardCode
      * @return array
      */
     public function applyGiftCardToCart();
+
+     /**
+     * Remove gift card from the cart.
+     * @param boolean $is_loggedin
+     * @param string|int $cartId
+     * @param string $giftCardCode
+     * @return array
+     */
+    public function removeGiftCardFromCart();
 }
--- Model/GiftCardAccountManagement.php
+++ Model/GiftCardAccountManagement.php
@@ -104,5 +104,57 @@ class GiftCardAccountManagement implements GiftCardAccountManagementInterface
         }
         return $response;
     }
+    public function removeGiftCardFromCart()
+    {
+        if ($this->moduleManager->isEnabled('Amasty_GiftCardAccount') && $this->moduleManager->isEnabled('Amasty_GiftCard')) {
+            $body          = $this->request->getBodyParams();
+            $giftCardCode  = $body['data']['gift_code'];
+            $maskedHashId  = $body['data']['cart_id'];
 
+            if(isset($giftCardCode) && isset($maskedHashId)) {
+                $accountRepository  = $this->objectManager->get("\Amasty\GiftCardAccount\Model\GiftCardAccount\Repository");
+                $gCardCartProcessor = $this->objectManager->get("\Amasty\GiftCardAccount\Model\GiftCardAccount\GiftCardCartProcessor");
+
+                $giftCardCode = trim($giftCardCode);
+                try {
+                    if (isset($body['data']['is_loggedin']) && $body['data']['is_loggedin']){
+                        $quote = $this->quoteRepository->getActive($maskedHashId);
+                    }else{
+                        $cartId = $this->maskedQuoteIdToQuoteId->execute($maskedHashId);
+                        $quote  = $this->quoteRepository->getActive($cartId);
+                    }
+                    $giftCard = $accountRepository->getByCode($giftCardCode);
+                    $gCardCartProcessor->removeFromCart($giftCard, $quote);
+                    $response = [
+                        [
+                            "code" => 'success',
+                            "message" => "Gift Card \"$giftCardCode\" was removed from the cart"
+                        ],
+                    ];
+                } catch (\Exception $e) {
+                    $response = [
+                        [
+                            "code" => 'error',
+                            "message" => $e->getMessage(),
+                        ],
+                    ];
+                }
+            } else {
+                $response = [
+                    [
+                        "code" => 'error',
+                        "message" => "Gift card extension not enable",
+                    ],
+                ];
+            }
+        } else {
+            $response = [
+                [
+                    "code" => 'error',
+                    "message" => "Gift card extension not enable",
+                ],
+            ];
+        }
+        return $response;
+    }
 }
