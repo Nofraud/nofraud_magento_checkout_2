3a9f093959c54e9c66e258175f6c0c0ed7c21524
Soleil Cotterell <soleilari@gmail.com>
2023-07-25 10:43:46 -0500

    ci: Add pre-release versioning to any release/* branch

---
+++ release.config.js
@@ -0,0 +1,50 @@
+const REF_NAME = process.env.GITHUB_REF_NAME;
+const MAIN_BRANCH = 'main';
+
+let config = {
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/github',
+    [
+      "@iwavesmedia/semantic-release-composer",
+      {
+        "skipOnMissingComposerJson": true
+      }
+    ]
+  ],
+  branches: [
+    {
+      name: MAIN_BRANCH,
+      prerelease: false,
+    },
+    {
+      name: '+([0-9])?(.{+([0-9]),x}).x',
+    },
+    {
+      name: 'release/*',
+      prerelease: 'RC',
+    },
+  ],
+};
+
+if (config.branches.some((it) => it.name === REF_NAME && !it.prerelease)) {
+  config.plugins.push(
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md',
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['composer.json','CHANGELOG.md'],
+        message:
+          'chore(release): ${nextRelease.version} \n\n${nextRelease.notes}',
+      },
+    ]
+  );
+}
+
+module.exports = config;
